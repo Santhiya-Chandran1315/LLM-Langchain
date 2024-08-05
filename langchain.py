549a7b96d98c14093750fa58a13b69d6ed118c2b
Santhiya-Chandran1315 <162896152+Santhiya-Chandran1315@users.noreply.github.com>
2024-08-05 13:01:11 +0530

    Add files via upload

---
+++ langchain.py
@@ -0,0 +1,12 @@
+import streamlit as st 
+
+def main():
+
+    st.set_page_config(page_title="Ask your CSV")
+    st.header("Ask your CSV")
+
+    user_csv = st.file_uploader("Upload your CSV file", type="csv")
+       
+            
+if __name__ == "__main__":
+    main()   
